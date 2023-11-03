5ab72b4d9b95fa5b3cfbbf5cdc2ee91bc69e68ef
adevreactjs <adevreactjs@gmail.com>
2023-11-03 12:17:06 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,11 @@ import PopularRequests from '@/components/popularRequests/PopularRequests';
 export default function Home() {
   return (
     <main className='bg-[#FBFBFB]'>
-<<<<<<< HEAD
       <NewsSlider title={'Новинки'} />
       <PopularRequests />
       <NewsSlider title={'Знижки'} width={450} discount={2244} fontSize={22} />
-=======
-      <NewsSlider title={'Новинки'} />
-      <PopularRequests/>
-      <NewsSlider title={'Знижки'} width={450} discount={2244} fontSize={22} />
       <OurBenefits />
       <FAQ />
->>>>>>> main
     </main>
   );
 }
