f265bdd15c36686efdefb6876d7f14a6475940b6
Ilkarion <Myfirststepsprogramming@gmail.com>
2023-11-03 09:32:57 +0200

    done with FAQ

---
+++ src/app/components/ServerSide/BenefitsCard.tsx
@@ -0,0 +1,7 @@
+export default function BenefitsCard() {
+    return(
+        <>
+            <BenefitsCard />
+        </>
+    )
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image';
 import Accordion from './components/ServerSide/Accordion';
 import FAQ from './components/ServerSide/FAQ';
+import OurBenefits from './components/ServerSide/OurBenefits';
 
 export default function Home() {
   return (
     <main className='bg-[#FBFBFB]'>
+      <OurBenefits />
       <FAQ />
     </main>
   );
