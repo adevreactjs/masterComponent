f96d9bcf27237a453b2b912c6f67786db1fd683d
adevreactjs <adevreactjs@gmail.com>
2023-11-05 19:53:59 +0200

    add styles

--- src/components/newSlider/NewsSlider.tsx
+++ src/components/newSlider/NewsSlider.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { FC, useRef, useState } from 'react';
+import React, { FC, useRef, useState, useEffect } from 'react';
 import NewsCart from '../newsCart/NewsCart';
 import cls from './index.module.scss';
 import leftArrow from '../../assets/leftArrow.svg';
@@ -24,6 +24,9 @@ const NewsSlider: FC<NewsSliderProps> = ({
   showDots,
 }) => {
   const [activeBreakLine, setActiveBreakLine] = useState(true);
+  const [currentWidth, setCurrentWidth] = useState(width);
+  const [currentFont, setCurrentFont] = useState(fontSize);
+
   const scrollRef = useRef<HTMLInputElement | null | any>(null);
 
   const scroll = (scrollOffset: number) => {
@@ -39,6 +42,17 @@ const NewsSlider: FC<NewsSliderProps> = ({
     setActiveBreakLine(false);
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      setCurrentWidth(window.innerWidth < 600 ? 328 : 450);
+      setCurrentFont(window.innerWidth < 600 ? 18 : 22);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [currentWidth, currentFont]);
+
   return (
     <div className={cls.newsSlider}>
       <h1 className={cls.title}>{title}</h1>
@@ -50,13 +64,13 @@ const NewsSlider: FC<NewsSliderProps> = ({
           <Image src={rightArrow} width={22} height={22} alt='rightArrow' />
         </button>
         <div className={cls.carts} ref={scrollRef}>
-          <NewsCart width={width} discount={discount} fontSize={fontSize} />
-          <NewsCart width={width} discount={discount} fontSize={fontSize} />
-          <NewsCart width={width} discount={discount} fontSize={fontSize} />
-          <NewsCart width={width} discount={discount} fontSize={fontSize} />
+          <NewsCart width={currentWidth} discount={discount} fontSize={currentFont} />
+          <NewsCart width={currentWidth} discount={discount} fontSize={currentFont} />
+          <NewsCart width={currentWidth} discount={discount} fontSize={currentFont} />
+          <NewsCart width={currentWidth} discount={discount} fontSize={currentFont} />
         </div>
-        {
-         showDots && <div className={cls.breakLines}>
+        {showDots && (
+          <div className={cls.breakLines}>
             <div
               onClick={breakLineHandlerFirst}
               className={`${activeBreakLine ? cls.activeBreakLine : cls.breakLine}`}></div>
@@ -64,7 +78,7 @@ const NewsSlider: FC<NewsSliderProps> = ({
               onClick={breakLineHandlerSecond}
               className={`${activeBreakLine ? cls.breakLine : cls.activeBreakLine}`}></div>
           </div>
-        }
+        )}
 
         <div className={cls.showMoreBtn}>
           {showMoreBtn && <button>Показати усі продукти</button>}
