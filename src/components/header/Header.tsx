4417039ff4c9fe60b223b75ed1a6de3c7970a5d0
adevreactjs <adevreactjs@gmail.com>
2023-11-06 14:25:50 +0200

    create mobileMenu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -16,30 +16,125 @@ import telIcon from '../../assets/telIcon.svg';
 import mailIcon from '../../assets/mailIcon.svg';
 import phoneIcon from '../../assets/phoneIcon.svg';
 import shopIcon from '../../assets/shopIcon.svg';
+import exitIcon from '../../assets/exitIcon.svg';
 import pointIcon from '../../assets/pointIcon.svg';
+import companyIcon from '../../assets/companyIcon.svg';
+import helpIcon from '../../assets/helpIcon.svg';
 import menu from '../../assets/menu.png';
+import catalogyIcon from '../../assets/catalogyIcon.svg';
 import { useState } from 'react';
 
 const Header = () => {
   const [activeMenu, setActiveMenu] = useState(false);
   const [activeHelpMenu, setHelpActiveMenu] = useState(false);
+  const [activeMobileMenu, setActiveMobileMenu] = useState(false);
   const [activeCategory, setActiveCategory] = useState(0);
 
   const openCategory = () => {
     setActiveMenu(!activeMenu);
     setActiveCategory(0);
-    setHelpActiveMenu(false)
+    setHelpActiveMenu(false);
   };
   const openHelpMenu = () => {
     setHelpActiveMenu(!activeHelpMenu);
-    setActiveMenu(false)
+    setActiveMenu(false);
   };
 
   return (
     <header className={cls.header}>
+      {activeMobileMenu && (
+        <section id={cls.mobileMenu}>
+          <ul>
+            <li>
+              <Image src={userIcon} width={16} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Профіль
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={favoriteIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Уподобані
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={catalogyIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Каталог
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={companyIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Про компанію
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={telIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Telegram
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={phoneIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Гаряча лінія
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={mailIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Email
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={shopIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Магазини
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={pointIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Пункти видачі
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={helpIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Допомога
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+            <li>
+              <Image src={exitIcon} width={21} height={20} alt='img' />
+              <div className={cls.mobileMenuContent}>
+                Вийти
+                <Image src={rightArrow} width={11} height={7} alt='img' />
+              </div>
+            </li>
+          </ul>
+        </section>
+      )}
       <div className={cls.container}>
         <div className={cls.wrapper}>
-          <Image className={cls.menu} src={menu} width={24} height={24} alt='menu' />
+          <Image
+            onClick={() => setActiveMobileMenu(!activeMobileMenu)}
+            className={cls.menu}
+            src={menu}
+            width={24}
+            height={24}
+            alt='menu'
+          />
           <div className={cls.logo}>MasterComponent</div>
           <nav className={cls.navBar}>
             <div className={cls.searchBar}>
