ea4c6dfc7f08355d80965fde6a41eb04ca1fbcd9
adevreactjs <adevreactjs@gmail.com>
2023-11-25 22:08:06 +0200

    add check to ProductQuestions

--- src/components/productCart/ProductQuestions.tsx
+++ src/components/productCart/ProductQuestions.tsx
@@ -4,6 +4,9 @@ import cls from './index.module.scss';
 import clipIcon from '@/assets/clip.svg';
 import avatarIcon from '@/assets/avatar.svg';
 import commentIcon from '@/assets/comment.svg';
+import mathImg from '@/assets/MathBord2.jpg';
+import favIcon from '@/assets/favIcon.svg';
+import productImg from '@/assets/mathCart.jpg';
 import { FC, ChangeEvent, useState, ReactNode } from 'react';
 import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 import { Message } from '@/types/type';
@@ -36,53 +39,84 @@ const ProductQuestions: FC = () => {
       file: image,
       date: formattedToday,
     };
-    setMessages(prev => [...prev, mess]);
-    setImage('');
-    console.log(messages);
+    if (message != '') {
+      setMessages(prev => [...prev, mess]);
+      setImage('');
+      setMessage('');
+    }
   };
 
   return (
     <>
       <div className={cls.productQuestions}>
-        <div className={cls.productQuestionsField}>
-          <div className={cls.questionsField}>
-            <input
-              type='text'
-              placeholder='Поставити запитання'
-              value={message}
-              onChange={e => setMessage(e.target.value)}
-            />
-            <label htmlFor='file-upload' className={cls.questionsFieldBtn}>
-              <input id='file-upload' type='file' accept='image/*' onChange={handleImageChange} />
-              <Image src={clipIcon} width={18} height={18} alt='clipIcon' />
-            </label>
-          </div>
-          <button className={cls.questionsSendBtn} onClick={sendQuestion}>
-            Надіслати
-          </button>
-        </div>
-      </div>
-      <div>
-        {messages.map((el, ind) => (
-          <div key={ind} className={cls.message}>
-            <div className={cls.avatar}>
-              <Image src={avatarIcon} width={30} height={30} alt='avatarIcon' />
-              <p>Степан</p>
+        <div>
+          <div className={cls.productQuestionsField}>
+            <div className={cls.questionsField}>
+              <input
+                type='text'
+                placeholder='Поставити запитання'
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+              />
+              <label htmlFor='file-upload' className={cls.questionsFieldBtn}>
+                <input id='file-upload' type='file' accept='image/*' onChange={handleImageChange} />
+                <Image src={clipIcon} width={18} height={18} alt='clipIcon' />
+              </label>
             </div>
-            <p className={cls.textMessage}>{el.message}</p>
-            {el.file != '' && <Image src={el.file} width={300} height={100} alt='image' />}
-            <div className={cls.infoMessage}>
-              <div className={cls.date}>{el.date}</div>
-              <div className={cls.comments}>
-                <div className={cls.commentsCount}>
-                  <Image src={commentIcon} width={15} height={15} alt='avatarIcon' />
-                  Коментарі: 1
+            <button className={cls.questionsSendBtn} onClick={sendQuestion}>
+              Надіслати
+            </button>
+          </div>
+          {messages.map((el, ind) => (
+            <div key={ind} className={cls.message}>
+              <div className={cls.avatar}>
+                <Image src={avatarIcon} width={30} height={30} alt='avatarIcon' />
+                <p>Степан</p>
+              </div>
+              <p className={cls.textMessage}>{el.message}</p>
+              {el.file != '' && <Image src={el.file} width={300} height={100} alt='image' />}
+              <div className={cls.infoMessage}>
+                <div className={cls.date}>{el.date}</div>
+                <div className={cls.comments}>
+                  <div className={cls.commentsCount}>
+                    <Image src={commentIcon} width={15} height={15} alt='avatarIcon' />
+                    Коментарі: 1
+                  </div>
+                  <div className={cls.commentsCount}>Відповісти </div>
                 </div>
-                <div className={cls.commentsCount}>Відповісти </div>
               </div>
             </div>
+          ))}
+        </div>
+
+        <div>
+          <div className={cls.productItem}>
+            <div className={cls.productImage}>
+              <Image
+                className={cls.productImg}
+                src={productImg}
+                width={542}
+                height={312}
+                alt='productImg'
+              />
+              <div className={cls.bonus}>+15 бонусів</div>
+              <button className={cls.favBtn}>
+                <Image src={favIcon} width={30} height={26} alt='favIcon' />
+              </button>
+            </div>
+            <div className={cls.description}>
+              <h2>Материнська плата Asus PRIME Z790-P</h2>
+              <div className={cls.price}>9 485 ₴</div>
+              <div className={cls.sale}>10 579 ₴</div>
+            </div>
+            <ul>
+              <li>(WIFI) D4</li>
+              <li>DDR4 для ПК</li>
+              <li>LGA1700</li>
+              <li>ATX</li>
+            </ul>
           </div>
-        ))}
+        </div>
       </div>
     </>
   );
