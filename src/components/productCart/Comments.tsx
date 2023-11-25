bd279dd6d9777b7ec25709a3d1a787b206a31f91
Ilkarion <Myfirststepsprogramming@gmail.com>
2023-11-25 09:11:48 +0200

    created mobile input

--- src/components/productCart/Comments.tsx
+++ src/components/productCart/Comments.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import magnifier from '../../assets/productCard/comments/magnifier.svg'
 import clip from '../../assets/productCard/comments/clip.svg'
+import pictureGrey from '../../assets/productCard/comments/PictureGrey.svg'
+import clipGrey from '../../assets/productCard/comments/clipGrey.svg'
+
 import { useSession } from "next-auth/react";
 export default function Comments() {
     const inputStyles = `
@@ -23,11 +26,18 @@ export default function Comments() {
     const { data: session } = useSession()
     return (
         <>
-            <div className="w-full border rounded-3xl overflow-hidden">
-                <div className="bg-[#C1C1C1] h-[25px]">
-                    <p>Hi {session?.user?.name}</p>
+            <div className="w-full border border-[#C1C1C1] rounded-3xl overflow-hidden">
+                <div className="bg-[#F1F1F1] h-[25px] flex flex-row items-center pl-5 pr-5 justify-between">
+                    <div className="flex flex-row items-center">
+                        <div className="bg-black w-8 h-8 rounded-full">{session?.user?.image}</div>
+                        <p className="text-[4vw] ml-2">Default_name{session?.user?.name}</p>
+                    </div>
+                    <div className="flex flex-row items-center">
+                        <Image src={pictureGrey} alt="picture" className="w-5 mr-[10px]"/>
+                        <Image src={clipGrey} alt="clip" className="w-5"/>
+                    </div>
                 </div>
-                <textarea name="comment" className="w-full pl-[2vw] pr-[2vw]" placeholder="Ваш відгук"></textarea>
+                <textarea name="comment" rows={5} className="w-full pl-[2vw] pr-[2vw] font-light placeholder:font-light placeholder:text-[14px] text-[14px] text-[#262626]" placeholder="Ваш відгук"></textarea>
             </div>
 
             <div className={`flex flex-row gap-10 ${responsiveBlockInput}`}>
